69c855b4f941b1847b0d449358bdcfb295ce730b
Krissa Santos <krissa.santos31@gmail.com>
2015-07-10 11:57:22 +0800

    Tried adding state to tasks but has undefined task error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,35 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-
-  # GET /tasks
-  # GET /tasks.json
+  respond_to :html
+ 
+ 
   def index
-    @tasks = Task.all
+  @tasks = current_user.tasks
+  respond_with(@tasks)
   end
+ 
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
+      respond_with(@task)
   end
 
-  # GET /tasks/new
+
   def new
     @task = Task.new
+    respond_with(@task)
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
+
   def create
-    @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    @task = current_user.tasks.new(task_params)
+    @task.save
+    respond_with(@task)
   end
-
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
-  def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+   
 
   # DELETE /tasks/1
   # DELETE /tasks/1.json
@@ -60,6 +40,7 @@ class TasksController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -71,4 +52,5 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:content)
     end
-end
+end 
+
